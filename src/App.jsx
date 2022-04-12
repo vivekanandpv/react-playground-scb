5f9b0606e31f0e641111ee9f33bda9905b31f389
Vivekanand P V <vivekanandpv@outlook.com>
2022-04-12 15:30:23 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <>
+      <div className='container p-5'>
+        <h3>Hello React!</h3>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default App;
