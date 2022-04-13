6a08cf4290e748ab66407e3494a5dbd878eec614
Vivekanand P V <vivekanandpv@outlook.com>
2022-04-13 09:01:50 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const App = (props) => {
-  let message = 'Good evening!';
-  const index = 1234;
-
-  const foo = () => 1234 * 8;
-
   return (
     <>
-      <h3 id={index * 9 + foo()}>My number is: {message + ' ' + foo()}</h3>
+      <div className='container p-5'>
+        <h3>App Component</h3>
+        <hr />
+      </div>
     </>
   );
 };
---
+++ src/components/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sample = (props) => {
+  return (
+    <>
+      <h3>Sample Component</h3>
+    </>
+  );
+};
+
+export default Sample;
